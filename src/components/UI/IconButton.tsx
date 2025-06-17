bf0ea53a3647b65b93af74c4b277468f878d19a1
Resham <reshamupadhyay48@gmail.com>
2025-06-17 22:40:34 +0530

    IconButton component

---
+++ src/components/UI/IconButton.tsx
@@ -0,0 +1,9 @@
+import type { ReactElement } from "react";
+
+interface IconButtonProps {
+    icon:ReactElement;
+    onClick?:() => void;
+}
+export default function IconButton({icon, onClick}:IconButtonProps) {
+  return <button className="bg-gradient-to-r from-purple-600 to-indigo-600 text-white hover:from-purple-700 hover:to-indigo-700 shadow-lg transform hover:scale-102 w-11 h-11 shrink-0 grow-0 rounded-full p-3" onClick={onClick}>{icon}</button>;
+}

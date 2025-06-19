242ed26804acdedf9e190cf691d0900cbf737e1f
Resham <reshamupadhyay48@gmail.com>
2025-06-19 22:33:56 +0530

    added pages

---
+++ src/pages/ChatPage.tsx
@@ -0,0 +1,58 @@
+import { LogOut, Send, Sofa } from "lucide-react";
+import Button from "../components/UI/Button";
+import Input from "../components/UI/Input";
+import IconButton from "../components/UI/IconButton";
+import { useContext, useRef } from "react";
+
+import MessageBubble from "../components/UI/MessageBubble";
+import { WebSocketContext } from "../context/wsContext";
+
+export default function ChatPage() {
+  const context = useContext(WebSocketContext);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const { messages, sendMessages, onlineCount, roomId, currentUsername } =
+    context;
+
+  function handleSend() {
+    const message = inputRef.current?.value.trim();
+    if (!message) return;
+
+    sendMessages(message);
+    inputRef.current.value = "";
+  }
+
+  return (
+    <div className="h-screen bg-gradient-to-br from-purple-100 via-white to-indigo-100 flex flex-col">
+      <div className="bg-white/80 backdrop-blur-sm border-b border-purple-100 px-6 py-4 shadow-sm flex justify-between">
+        <div className="flex items-center gap-3">
+          <div className="w-10 h-10 bg-gradient-to-r from-purple-600 to-indigo-600 rounded-full flex items-center justify-center">
+            <Sofa className="text-white" />
+          </div>
+          <div className="flex flex-col">
+            <h1 className="text-xl font-semibold text-gray-900">{roomId}</h1>
+            <p className="text-sm text-gray-500">{onlineCount} online</p>
+          </div>
+        </div>
+        <div>
+          <Button text="Leave" variant="secondary" startIcon={<LogOut />} />
+        </div>
+      </div>
+
+      <div className="flex-1 overflow-y-auto px-6 py-4 space-y-4">
+        {messages.map((m, i) => (
+          <MessageBubble
+            key={i}
+            message={m.text}
+            username={m.username}
+            isOwn={m.username === currentUsername}
+          />
+        ))}
+      </div>
+
+      <div className="bg-white/80 border-t bordeer-purple-100 px-6 py-4 flex gap-5">
+        <Input placeholder="Type a message......." refrance={inputRef} />
+        <IconButton icon={<Send />} onClick={handleSend} />
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/CreateChatRoom.tsx
@@ -0,0 +1,62 @@
+import { useContext, useRef, useState } from "react";
+import Button from "../components/UI/Button";
+import Input from "../components/UI/Input";
+import { WebSocketContext } from "../context/wsContext";
+import { useNavigate } from "react-router-dom";
+
+
+export default function CreateChatRoom() {
+  const [newCode, setNewCode] = useState();
+  const roomInputId = useRef();
+  const usernameRef = useRef();
+const navigate = useNavigate();
+  const context = useContext(WebSocketContext)
+
+  if(!context){
+    console.log("no access to context")
+  }
+
+  const { connect } = context;
+
+  function handleJoin() {
+    const roomId = roomInputId.current?.value;
+    const username = usernameRef.current?.value;
+    if (roomId) {
+      connect(roomId,username);
+      navigate("/chat")
+    }
+  }
+
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-purple-100 via-white to-indigo-100 flex items-center justify-center p-4">
+      <div className="rounded-2xl shadow-xl border bg-white/70 border-purple-100 p-8 w-full max-w-md space-y-6">
+        <div className="text-center space-y-2">
+          <h1 className="text-3xl font-bold text-gray-800 mb-2">Welcome</h1>
+          <p className="text-gray-600 text-sm">Connect instantly with friends and family</p>
+        </div>
+        
+        <Input placeholder="Enter Name" refrance={usernameRef}  />
+
+        <div className="flex items-center justify-center  gap-4">
+          <Input placeholder="Room code" refrance={roomInputId} />
+          <Button text="Join" size="md" onClick={handleJoin}  />
+        </div>
+
+        <div className="pt-3 space-y-5">
+          <Button
+            text="Create a new room"
+            fullWidth={true}
+            onClick={() => {
+              setNewCode(Math.floor(100000 + Math.random() * 900000));
+            }}
+          />
+          {newCode && (
+            <div className="text-center text-lg text-gray-600 w-full bg-gray-200 px-3 py-6 rounded-xl">
+              {newCode}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}

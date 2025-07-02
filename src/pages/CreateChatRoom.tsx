43ec2a1f98e4f1481e4f77eb0bb0c13636ea6f0d
Resham <reshamupadhyay48@gmail.com>
2025-07-02 11:41:39 +0530

    utilitis

--- src/pages/CreateChatRoom.tsx
+++ src/pages/CreateChatRoom.tsx
@@ -3,6 +3,7 @@ import Button from "../components/UI/Button";
 import Input from "../components/UI/Input";
 import { WebSocketContext } from "../context/wsContext";
 import { useNavigate } from "react-router-dom";
+import { generateRoomId } from "../utils/roomUtils";
 
 
 export default function CreateChatRoom() {
@@ -31,7 +32,7 @@ const navigate = useNavigate();
     <div className="min-h-screen bg-gradient-to-br from-purple-100 via-white to-indigo-100 flex items-center justify-center p-4">
       <div className="rounded-2xl shadow-xl border bg-white/70 border-purple-100 p-8 w-full max-w-md space-y-6">
         <div className="text-center space-y-2">
-          <h1 className="text-3xl font-bold text-gray-800 mb-2">Welcome</h1>
+          <h1 className="text-3xl font-bold text-gray-800 mb-2">Get Started</h1>
           <p className="text-gray-600 text-sm">Connect instantly with friends and family</p>
         </div>
         
@@ -47,7 +48,7 @@ const navigate = useNavigate();
             text="Create a new room"
             fullWidth={true}
             onClick={() => {
-              setNewCode(Math.floor(100000 + Math.random() * 900000));
+              setNewCode( generateRoomId());
             }}
           />
           {newCode && (
